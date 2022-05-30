8d3b325b0b0d4ed97d86b72e4b2becfbca175c55
zwx1058539 <zhenghui17@huawei.com>
2022-05-30 17:15:27 +0800

    优化数据表文档

---
+++ mall/document/sql/anonymous/one_mall/product_attr_key.sql
@@ -0,0 +1,14 @@
+create table if not exists product_attr_key
+(
+    id          int auto_increment comment '规格编号'
+        primary key,
+    name        varchar(32)   not null comment '规格名称',
+    status      int default 1 not null comment '状态：0->关闭;1->开启;',
+    deleted     int default 1 not null comment '是否被删除：0->已删除;1->未删除;',
+    create_time datetime      not null comment '创建时间',
+    update_time datetime      not null comment '更新时间',
+    constraint product_attr_key_id_uindex
+        unique (id)
+)
+    comment '商品规格表' charset = utf8mb4;
+
---
+++ mall/document/sql/anonymous/one_mall/product_attr_value.sql
@@ -0,0 +1,15 @@
+create table if not exists product_attr_value
+(
+    id          int auto_increment comment '规格值编号'
+        primary key,
+    attrKeyId   int           not null comment '规格编号',
+    name        varchar(32)   not null comment '规格值名字',
+    status      int default 1 not null comment '状态：0->关闭;1->开启;',
+    deleted     int default 1 not null comment '是否被删除：0->已删除;1->未删除;',
+    create_time datetime      not null comment '创建时间',
+    update_time datetime      not null comment '更新时间',
+    constraint product_attr_value_id_uindex
+        unique (id)
+)
+    comment '商品规格值表' charset = utf8mb4;
+
