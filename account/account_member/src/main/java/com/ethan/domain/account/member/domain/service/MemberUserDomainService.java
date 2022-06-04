6496ca3f7b362c0d74b131eeb23125d5c21e6faa
zhenghui <15270855286@163.com>
2022-06-04 14:41:53 +0800

    会员用户表结构设计

---
+++ account/account_member/src/main/java/com/ethan/domain/account/member/domain/service/MemberUserDomainService.java
@@ -0,0 +1,12 @@
+package com.ethan.domain.account.member.domain.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhenghui
+ * @Description 会员用户领域服务
+ * @Date 2022/6/4
+ */
+@Service
+public class MemberUserDomainService {
+}
